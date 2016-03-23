feb0d72e856cae4c46a1ed02e738ed9bbc7a4eb1
Augusts Bautra <augusts.bautra@gmail.com>
2016-03-23 14:42:12 +0200

    Minor clean

--- db/migrate/20151106101824_create_development_notification_emails.rb
+++ db/migrate/20151106101824_create_development_notification_emails.rb
@@ -1,6 +1,5 @@
 class CreateDevelopmentNotificationEmails < ActiveRecord::Migration
   def change
-    return if table_exists?(:development_notification_emails)
     create_table :development_notification_emails do |t|
       t.string :title, index: true, null: false
       t.string :to_address, index: true, null: false
@@ -10,6 +9,6 @@ class CreateDevelopmentNotificationEmails < ActiveRecord::Migration
       t.integer :status, index: true, null: false, default: 0
 
       t.timestamps
-    end
+    end unless table_exists?(:development_notification_emails)
   end
 end
